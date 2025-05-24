e9f2318bc08a97894ad458431a7b254a304d5d84
Shubham Chaturvedi <shubhamchat224122@gmail.com>
2025-05-24 12:49:42 +0530

    added new cotent and resume

--- src/jobdata.js
+++ src/jobdata.js
@@ -5,17 +5,19 @@ export const jobsData = [
     roleName: "Software Developer Engineer",
     workExperience: [
       [
-        { text: "• Integrated Slack Webhooks across all products (" },
+        {
+          text:"• Created and implemented scalable AWS infrastructure using Golang that can handle 400K+ reservations per month with a response time of less than 200 ms and a 99.9% uptime rate."
+        }
+      ],
+      [
+        { text: "• Reduced response time from 24 hours to 1 hour by integrating Slack Webhooks into all products (" },
         { text: "Health", link: "https://health.covrzy.com" },
         { text: ", " },
         { text: "Liability", link: "https://start.covrzy.com" },
-        { text: ") to enable zero-delay client request routing to the sales team, reducing response time from 24 hours to 1 hour." }
-      ],
-      [
-        { text: "• Improved company webpage performance through advanced React optimization techniques, integrated Strapi CMS for " },
-        { text: "Blog", link: "https://covrzy.com/blog" },
-        { text: " automation, and implemented email support using the MSG91 API." }
-      ],
+        { text: ") to allow for zero-delay client request routing to the sales team. Integrated the MSG91 email API and Heltar WhatsApp API to optimize communication processes and reduce client response times to almost instantaneous levels, and used " },
+        { text: "Strapi CMS", link: "https://covrzy.com/blog" },
+        { text: " for blog automation." }
+      ],      
       [
         { text: "• Developed scalable and maintainable web applications using Node.js, TypeScript, and React, ensuring efficiency and user-focused design." }
       ]
